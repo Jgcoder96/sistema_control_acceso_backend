6531d4e04e65fe5d6eff68d6eabe6d0a924a67aa
JGCoder96 <jgcoder96@gmail.com>
2025-12-09 17:38:15 -0400

    Finalizada la configuración base de WebSockets en el backend #1

--- src/app/setup/setupErrors.ts
+++ src/app/setup/setupErrors.ts
@@ -1,6 +1,6 @@
 import type { Express, Request, Response } from 'express';
 
-export const setupError = (app: Express) => {
+export const setupError = (app: Express): void => {
   app.use((_req: Request, res: Response) => {
     res.status(404).json({
       res: false,
---
+++ src/websocket/index.ts
@@ -0,0 +1,15 @@
+import { Server } from 'socket.io';
+import { Server as ServerHttp } from 'http';
+
+import { startEventListener } from './events/startEventListener.js';
+
+export const initWss = (server: ServerHttp): Server => {
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  });
+  startEventListener(io);
+  return io;
+};
