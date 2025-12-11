5277ab86fc2126de2f3c40f9bf0fd24022aee8e2
JGCoder96 <jgcoder96@gmail.com>
2025-12-11 14:19:06 -0400

    Finalizada la configuración inicial de CORS de Express en el servidor #3

--- src/app/setup/setupMiddleware.ts
+++ src/app/setup/setupMiddleware.ts
@@ -1,8 +1,12 @@
 import type { Express } from 'express';
 
 import express from 'express';
+import cors from 'cors';
+
+import { corsOptions } from '../config/corsOptions.config.js';
 
 export const setupMiddleware = (app: Express): Express => {
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
